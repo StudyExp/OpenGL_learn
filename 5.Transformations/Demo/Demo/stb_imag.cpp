fadfbc3a79164d144b433a469699929a52e3d157
cywd <lichunyang@outlook.com>
2018-07-24 15:21:53 +0800

    cc

---
+++ 5.Transformations/Demo/Demo/stb_imag.cpp
@@ -0,0 +1,11 @@
+//
+//  stb_imag.cpp
+//  Demo
+//
+//  Created by cyrill on 2018/7/19.
+//  Copyright © 2018年 cyrill.win. All rights reserved.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
